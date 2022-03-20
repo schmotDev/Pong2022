ff40bf02be05286e5e6c8c37b271d2f61c748a58
schmotDev <89300244+schmotDev@users.noreply.github.com>
2022-03-20 12:28:15 +0100

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,6 +1,14 @@
 -- JEU: Pong 2022 
+love.graphics.setDefaultFilter("nearest")
 
-RAQUETTE = {largeur=10, hauteur=80}
+raquette = {}
+raquette.image = love.graphics.newImage("images/pong.png")
+raquette.frames = {}
+raquette.frames[1] = love.graphics.newQuad(65,0,46,8,raquette.image:getWidth(),raquette.image:getHeight())
+raquette.frame = 1
+raquette.type = 0 -- raquette normale
+raquette.largeur = 10
+raquette.hauteur = 80
 
 limiteDROITE = love.graphics.getWidth()
 limiteGAUCHE= 0
@@ -10,11 +18,11 @@ screenWidth = limiteDROITE - limiteGAUCHE
 screenHeight = limiteBAS - limiteHAUT
 
 player1Posx = 0
-player1Posy = screenHeight/2 - RAQUETTE.hauteur/2 + limiteHAUT
+player1Posy = screenHeight/2 - raquette.hauteur/2 + limiteHAUT
 scorePlayer1 = 0
 
 player2Posx = screenWidth
-player2Posy = screenHeight/2 - RAQUETTE.hauteur/2 + limiteHAUT
+player2Posy = screenHeight/2 - raquette.hauteur/2 + limiteHAUT
 scorePlayer2 = 0
 
 pause = true
@@ -27,14 +35,15 @@ balle.x = 0
 balle.y = 0
 balle.vitesseX = 2
 balle.vitesseY = 2
-
-sndMur = love.audio.newSource("mur.wav", "static")
-sndPerdu = love.audio.newSource("perdu.wav", "static")
+balle.image = love.graphics.newImage("images/balls.png")
+balle.frames = {}
+balle.frames[1] = love.graphics.newQuad(68,0, 14, 14, balle.image:getWidth(),balle.image:getHeight())
 
 trail = {}
 impact = {}
 explosion = {}
 
+machin = 0
 
 function BalleCentre()
   balle.x = screenWidth / 2 - balle.LARGEUR / 2
@@ -89,22 +98,22 @@ function DeplaceBalle(dt)
   
   if (balle.x + balle.LARGEUR) > limiteDROITE then
     Point("PLAYER 1")
-    love.audio.play(sndPerdu)
+    sndPerdu:play()
   end
   if (balle.x) <= limiteGAUCHE then
     Point("PLAYER 2")
-    love.audio.play(sndPerdu)
+    sndPerdu:play()
   end
   
   balle.y = balle.y + balle.vitesseY
   
   if (balle.y + balle.HAUTEUR) > limiteBAS then
     balle.vitesseY  = balle.vitesseY * -1
-    love.audio.play(sndMur)
+    sndMur:play()
   end
   if (balle.y) <= limiteHAUT then
     balle.vitesseY  = balle.vitesseY * -1
-    love.audio.play(sndMur)
+    sndMur:play()
   end
 end
 
@@ -128,6 +137,9 @@ function Point(joueur)
   pause = true
   point = true
   
+  musicIntro:play()
+  --love.audio.play(musicIntro)
+  
   if scorePlayer1 == 10 or scorePlayer2 == 10 then
     affichage = joueur.." IS THE WINNER !!!! PRESS SPACE TO RESTART"
     scorePlayer1 = 0
@@ -138,26 +150,26 @@ end
 
 function Collision()
   
-  if balle.x <= (player1Posx + RAQUETTE.largeur) then
-    if (balle.y <= (player1Posy + RAQUETTE.hauteur)) and (balle.y >= (player1Posy - balle.HAUTEUR)) then
-      balle.x = player1Posx + RAQUETTE.largeur
+  if balle.x <= (player1Posx + raquette.largeur) then
+    if (balle.y <= (player1Posy + raquette.hauteur)) and (balle.y >= (player1Posy - balle.HAUTEUR)) then
+      balle.x = player1Posx + raquette.largeur
       balle.vitesseX  = balle.vitesseX * -1
-      love.audio.play(sndMur)
+      sndRaq:play()
     end
   end
   
-  if (balle.x + balle.LARGEUR) >= (player2Posx - RAQUETTE.largeur) then
-    if (balle.y <= (player2Posy + RAQUETTE.hauteur)) and (balle.y >= (player2Posy - balle.HAUTEUR)) then
-      balle.x = player2Posx - RAQUETTE.largeur - balle.LARGEUR
+  if (balle.x + balle.LARGEUR) >= (player2Posx - raquette.largeur) then
+    if (balle.y <= (player2Posy + raquette.hauteur)) and (balle.y >= (player2Posy - balle.HAUTEUR)) then
+      balle.x = player2Posx - raquette.largeur - balle.LARGEUR
       balle.vitesseX  = balle.vitesseX * -1
-      love.audio.play(sndMur)
+      sndRaq:play()
     end
   end
 end
 
 function DeplaceJoueur()
   
-  if love.keyboard.isDown("a") and ((player1Posy+RAQUETTE.hauteur) <= limiteBAS) then
+  if love.keyboard.isDown("a") and ((player1Posy+raquette.hauteur) <= limiteBAS) then
     player1Posy = player1Posy + 2
   end
   
@@ -165,7 +177,7 @@ function DeplaceJoueur()
     player1Posy = player1Posy - 2
   end
   
-  if love.keyboard.isDown("down") and ((player2Posy+RAQUETTE.hauteur) <= limiteBAS) then
+  if love.keyboard.isDown("down") and ((player2Posy+raquette.hauteur) <= limiteBAS) then
     player2Posy = player2Posy + 2
   end
   if love.keyboard.isDown("up") and player2Posy > limiteHAUT then
@@ -175,6 +187,8 @@ function DeplaceJoueur()
   if love.keyboard.isDown("space") and player2Posy > limiteHAUT then
     pause = false
     point = false
+    
+    love.audio.stop(musicIntro)
   end
   
 end
@@ -183,10 +197,22 @@ function screenStart()
   affichage = "PUSH SPACE TO START THE GAME"
   pause = true
   
+  musicIntro:play()
+  --love.audio.play(musicIntro)
+  
 end
 
 function love.load()
   love.window.setTitle("PONG 2022")
+  
+  sndMur = love.audio.newSource("sounds/mur2.wav", "static")
+  sndPerdu = love.audio.newSource("sounds/perdu.wav", "static")
+  sndRaq = love.audio.newSource("sounds/sonRaq.wav", "static")
+  
+  musicIntro = love.audio.newSource("music/intro.wav", "stream")
+  musicIntro:setLooping(true)
+  
+
   BalleCentre()
   screenStart()
 end
@@ -201,26 +227,31 @@ function love.update(dt)
     ExploseBalle(dt)
   end
   
+  
   --if point then Point() end
 end
 
 function love.draw()
+  
   love.graphics.rectangle("line", 0,0, limiteDROITE,limiteHAUT-1)
   love.graphics.print("PLAYER ONE", 10,10)
   love.graphics.print("SCORE: "..scorePlayer1, 10,25)
   love.graphics.print("PLAYER TWO", (limiteDROITE-100),10)
   love.graphics.print("SCORE: "..scorePlayer2, (limiteDROITE-100),25)
 
-  love.graphics.rectangle("fill", player1Posx,player1Posy, RAQUETTE.largeur,RAQUETTE.hauteur)
-  love.graphics.rectangle("fill", player2Posx-RAQUETTE.largeur,player2Posy, RAQUETTE.largeur,RAQUETTE.hauteur)
+  --love.graphics.rectangle("fill", player1Posx+10,player1Posy, raquette.largeur,raquette.hauteur)
+  love.graphics.rectangle("fill", player2Posx-raquette.largeur,player2Posy, raquette.largeur,raquette.hauteur)
+  love.graphics.draw(raquette.image, raquette.frames[1], player1Posx,player1Posy+80, math.rad(-90), 80/46, 10/8)
   
   for n=1, #trail do
     local t = trail[n]
     love.graphics.setColor(1,1,1, t.vie/2)
-    love.graphics.rectangle("fill", t.x,t.y, balle.LARGEUR,balle.HAUTEUR)
+    love.graphics.circle("fill", t.x,t.y, balle.LARGEUR/2)
+    --love.graphics.draw(balle.image, balle.frames[1], balle.x, balle.y, 0, 1,1, 5, 5)
   end  
   love.graphics.setColor(1,1,1, 1)
-  love.graphics.rectangle("fill", balle.x,balle.y, balle.LARGEUR,balle.HAUTEUR)
+  --love.graphics.rectangle("fill", balle.x,balle.y, balle.LARGEUR,balle.HAUTEUR)
+  love.graphics.draw(balle.image, balle.frames[1], balle.x, balle.y, 0, 1,1, 5, 5)
   
   if pause then
     local font = love.graphics.getFont()
